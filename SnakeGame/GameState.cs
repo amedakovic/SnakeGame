4120dafc501e98e1f94d265d39985c4dc65edbe0
amedakovic <medakovic.aleks1@gmail.com>
2023-03-16 14:16:17 +0100

    Complited

---
+++ SnakeGame/GameState.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
+        private readonly Random random = new Random();
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
+
+        public GameState(int rows, int col)
+        {
+            Rows = rows;
+            Columns = col;
+            Grid = new GridValue[rows, col];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            for(int c = 1; c < 4; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePosition.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> GetEmptyPosition()
+        {
+            for(int r = 0;r < Rows; r++)
+            {
+                for(int c = 0; c < Columns; c++)
+                {
+                    if(Grid[r,c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood()
+        {
+            List<Position> emptyPosition = new List<Position>(GetEmptyPosition());
+            if(emptyPosition.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = emptyPosition[random.Next(emptyPosition.Count)];
+            Grid[pos.Row, pos.Column] = GridValue.Food;
+        }
+
+        public Position GetSnakeHeadPosition()
+        {
+            return snakePosition.First.Value;
+        }
+
+        public Position GetTailPosition()
+        {
+            return snakePosition.Last.Value;
+        }
+
+        public IEnumerable<Position> GetSnakePosition()
+        {
+            return snakePosition;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePosition.AddFirst(pos);
+            Grid[pos.Row, pos.Column] = GridValue.Snake;
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePosition.Last.Value;
+            Grid[tail.Row, tail.Column] = GridValue.Empty;
+            snakePosition.RemoveLast();
+        }
+
+        private Direction GetLasDirection()
+        {
+            if(dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+            return dirChanges.Last.Value;
+        }
+
+        private bool canChangeDirection(Direction newDir)
+        {
+            if(dirChanges.Count == 2)
+            {
+                return false;
+            }
+            Direction lastdir = GetLasDirection();
+            return newDir != lastdir && newDir != lastdir.Opposite();
+        }
+        public void ChangeDirection(Direction dir)
+        {
+            if (canChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
+            
+        }
+
+        private bool OutsideGrid(Position pos)
+        {
+            return pos.Row < 0 || pos.Row >= Rows || pos.Column < 0 || pos.Column >= Columns;
+        }
+
+        private GridValue WillHit(Position newHeadPos)
+        {
+            if (OutsideGrid(newHeadPos))
+            {
+                return GridValue.Outside;
+            }
+
+            if(newHeadPos == GetTailPosition())
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Column];
+        }
+
+        public void Move()
+        {
+            if(dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+            Position newHeadPos = GetSnakeHeadPosition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if(hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                GameOver = true;
+            }
+            else if(hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if(hit == GridValue.Food)
+            {
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+        }
+    }
+}
--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -20,9 +20,150 @@ namespace SnakeGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool gameRunning = false;
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new Dictionary<GridValue, ImageSource>()
+        {
+            {GridValue.Empty, Images.Empty },
+            {GridValue.Snake, Images.Body },
+            {GridValue.Food, Images.Food }
+        };
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
         public MainWindow()
         {
+            gameState = new GameState(rows,cols);
             InitializeComponent();
+            gridImages = SetUpGrid();
+
+        }
+
+        private async Task RunGame()
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            await GameLoop();
+            ShowGameOver();
+            gameState = new GameState(rows,cols);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+            switch (e.Key)
+            {
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+
+        public async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(120);
+                gameState.Move();
+                Draw();
+            }
+        }
+        public Image[,] SetUpGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for(int r = 0; r < rows; r++)
+            {
+                for(int c = 0; c< cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
+        }
+
+        private void Draw()
+        {
+            ScoreText.Text = $"Score: {gameState.Score}";
+            DrawGrid();
+        }
+
+        
+
+        private void DrawGrid()
+        {
+            for(int r = 0; r< rows; r++)
+            {
+                for(int c = 0; c< cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                }
+            }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for(int i = 3; i > 0; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(1000);
+            }
+        }
+    
+        private async Task ShowGameOver()
+        {
+            await DrawDeadSnake();
+            await Task.Delay(100);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "Press any key to start";
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.GetSnakePosition());
+            for(int i = 0; i< positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = Images.DeadBody;
+                gridImages[pos.Row, pos.Column].Source = source;
+                await Task.Delay(50);
+            }
         }
     }
 }
